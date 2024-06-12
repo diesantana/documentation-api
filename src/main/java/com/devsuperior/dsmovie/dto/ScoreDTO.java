32a7926f8eaa172906f98da8d5686277f81e1d49
Diego Santana <3c.santana@gmail.com>
2024-06-12 07:33:03 -0300

    feat: Customiza a documentação do ScoreDTO

--- src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
+++ src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -4,6 +4,7 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
@@ -12,9 +13,11 @@ public class ScoreDTO {
 	private static final DecimalFormat df = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
 
 	@NotNull(message = "Required field")
+	@Schema(description = "Movie id (required)")
 	private Long movieId;
 
 	@PositiveOrZero(message = "Score should be greater than or equal to zero")
+	@Schema(description = "Movie score")
 	private Double score;
 
 	public ScoreDTO(Long movieId, Double score) {
